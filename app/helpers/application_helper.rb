7f1103952605e9aaf2554d1be4627560aa551eb5
Chris Siegman <chrissiegman@yahoo.com>
2015-11-15 22:59:36 +0000

    Add title helper function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  # Returns full page title for each page
+  def full_title(page_title = '')
+    base_title = "ListsApp"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+  
 end
