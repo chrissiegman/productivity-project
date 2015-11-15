ccdf88d1cb87116a71f2b6b9b9baed00102a234a
chrissiegman <chrissiegman@yahoo.com>
2015-11-15 21:49:58 +0000

    Add unit test for About page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,5 +10,9 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :help
     assert_response :success
   end
-
+  
+  test "should get about" do
+    get :help
+    assert_response :success
+  end
 end
