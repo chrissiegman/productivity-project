ce20482bd9302223c9579901388b7ee6d0733361
Chris Siegman <chrissiegman@yahoo.com>
2015-11-16 02:30:15 +0000

    Rename and tweak site

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   # Returns full page title for each page
   def full_title(page_title = '')
-    base_title = "ListsApp"
+    base_title = "To-Do Lists App"
     if page_title.empty?
       base_title
     else
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,24 +4,24 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "ListsApp"
+    assert_select "title", "To-Do Lists App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | ListsApp"
+    assert_select "title", "Help | To-Do Lists App"
   end
   
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | ListsApp"
+    assert_select "title", "About | To-Do Lists App"
   end
   
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | ListsApp"
+    assert_select "title", "Contact | To-Do Lists App"
   end
 end
