9518678b3136d86e206fb0e4ee64c3b7f5bb37f5
Chris Siegman <chrissiegman@yahoo.com>
2015-11-18 22:19:14 +0000

    Finish login/logout

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -71,4 +71,9 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = ' ' * 10
     assert_not @user.valid?
   end
+  
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
+  
 end
